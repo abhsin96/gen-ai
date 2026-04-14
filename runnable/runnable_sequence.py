e7ffa071ea809ec078838bbfba0bd4393ef5d57e
abhsin96 <abhsin96@gmail.com>
2026-04-14 20:41:55 +0530

    Add runnable sequence implementation for generating and summarizing facts

---
+++ runnable/runnable_sequence.py
@@ -0,0 +1,32 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+# First prompt: Generate facts about a topic
+prompt1 = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+# Second prompt: Summarize and enhance the facts
+prompt2 = PromptTemplate(
+    template="Take these facts and create a brief, engaging summary:\n\n{facts}",
+    input_variables=["facts"]
+)
+
+# Create a sequence with 2 prompts
+chain1 = RunnableSequence(prompt1, model, parser,prompt2, model, parser)
+
+result = chain1.invoke({
+    "topic": "Laughing"
+})
+
+print(result)
